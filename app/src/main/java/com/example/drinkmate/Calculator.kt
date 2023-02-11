9fcf96fdbb0d9724ae8cfb1218de953da3e35c31
ThomsonNguyen2001 <Thomson.Nguyen2001@gmail.com>
2023-02-10 22:28:19 -0800

    Changed to using Navigation component

---
+++ app/src/main/java/com/example/drinkmate/Calculator.kt
@@ -0,0 +1,23 @@
+package com.example.drinkmate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Calculator.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Calculator : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_calculator, container, false)
+        return view
+    }
+}
