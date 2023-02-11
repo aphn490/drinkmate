e0328be8e295711d9f37d545e93c8cb1c013b040
Mark G <mark.garcia.8001@gmail.com>
2023-02-10 23:01:10 -0800

    work

--- app/src/main/java/com/example/drinkmate/Login.kt
+++ app/src/main/java/com/example/drinkmate/Login.kt
@@ -13,18 +13,60 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.drinkmate.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
     private lateinit var createAcc: Button
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
+    private lateinit var login: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var textView: TextView
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
         createAcc = findViewById(R.id.CA)
         createAcc.setOnClickListener{
             val intent = Intent(this, Registration::class.java)
             startActivity(intent)
+        }
+
+        login.setOnClickListener{
+            val em = email.text.toString()
+            val pas = password.text.toString()
+            if (em.isNotEmpty() && pas.isNotEmpty()){
+                auth.signInWithEmailAndPassword(em, pas)
+                    .addOnCompleteListener() { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText( this@Login, "Login Successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText( this@Login, "Authentication Failed", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            } else {
+                Toast.makeText( this@Login, "Empty Fields Are Not Allowed", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
--- app/src/main/java/com/example/drinkmate/Registration.kt
+++ app/src/main/java/com/example/drinkmate/Registration.kt
@@ -1,8 +1,11 @@
 package com.example.drinkmate
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drinkmate.databinding.ActivityMainBinding
@@ -19,17 +22,24 @@ class Registration : AppCompatActivity() {
     private lateinit var confPass: TextInputEditText
     private lateinit var confButton: Button
     private lateinit var auth: FirebaseAuth
+    private lateinit var textView: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         // set variables to what is currently in the edit text box in activity.
-        this.email = findViewById(R.id.email)
-        this.password = findViewById(R.id.password)
-        this.confPass = findViewById(R.id.confirmPassword)
-        this.confButton = findViewById(R.id.Create_Account)
-        this.auth = FirebaseAuth.getInstance()
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        confPass = findViewById(R.id.confirmPassword)
+        confButton = findViewById(R.id.Create_Account)
+        auth = FirebaseAuth.getInstance()
+        textView = findViewById(R.id.loginNow)
+
+        textView.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
 
         confButton.setOnClickListener {
             val em = email.text.toString()
@@ -39,20 +49,13 @@ class Registration : AppCompatActivity() {
             if (em.isNotEmpty() && pas.isNotEmpty() && cpas.isNotEmpty()) {
                 if (pas == cpas) {
                     auth.createUserWithEmailAndPassword(em, pas)
-                        .addOnCompleteListener(this) { task ->
+                        .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
                                 // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "createUserWithEmail:success")
-                                val user = auth.currentUser
-                                updateUI(user)
+                                Toast.makeText( this@Registration, "Account Created", Toast.LENGTH_SHORT).show()
                             } else {
                                 // If sign in fails, display a message to the user.
-                                Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                                Toast.makeText(
-                                    baseContext, "Authentication failed.",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                updateUI(null)
+                                Toast.makeText( this@Registration, "Authentication Failed", Toast.LENGTH_SHORT).show()
                             }
                         }
                 } else {
@@ -63,26 +66,6 @@ class Registration : AppCompatActivity() {
                 Toast.makeText( this@Registration, "Empty Fields Are Not Allowed", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-
-    private fun regAccount(em: String, pw: String, cpw:String) {
-        val db = FirebaseFirestore.getInstance()
-        val user: MutableMap<String, Any> = HashMap()
-        if (em.isNotEmpty() && pw.isNotEmpty() && cpw.isNotEmpty()) {
-            if (pw == cpw) {
-                user["email"] = em
-                user["password"] = pw
-                db.collection("UserAccounts")
-                    .add(user)
-                    .addOnSuccessListener {
-                        Toast.makeText(this@Registration,"Account Created Successfully",Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this@Registration,"Error Adding Record",Toast.LENGTH_SHORT).show()
-                    }
-            }
-        } else
-            Toast.makeText( this@Registration, "Empty Fields Are Not Allowed", Toast.LENGTH_SHORT).show()
     }
 
 }
