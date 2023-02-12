50baed5661828e6e8830692527cc5e5b7540c3c9
William Pham <55603252+willpham@users.noreply.github.com>
2023-02-11 20:37:59 -0800

    Bar Map Implementation - MapsActivity KT and activity_maps XML

--- app/src/main/java/com/example/drinkmate/CustomInfoWindowAdapter.kt
+++ app/src/main/java/com/example/drinkmate/CustomInfoWindowAdapter.kt
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.model.Marker
 import com.squareup.picasso.Picasso
 
 //CustomInfoWindowAdapter Class to create an implementation of the InfoWindowAdapter interface to create a custom info window for Google Map markers
-class CustomInfoWindowAdapter: GoogleMap.InfoWindowAdapter  {
+class CustomInfoWindowAdapter(private val context: Context): GoogleMap.InfoWindowAdapter  {
     //Associate this class with the custom_info_window XML layout created to override the default market info window, take the XML layout and parse it to the view
     private val infoWindow: View = LayoutInflater.from(context).inflate(R.layout.custom_info_window, null)
 
@@ -20,6 +20,7 @@ class CustomInfoWindowAdapter: GoogleMap.InfoWindowAdapter  {
 
         //Set image for the custom info window with image_url from Bar Object (retrieved from Yelp API)
         val barImageView = infoWindow.findViewById<ImageView>(R.id.barImageView)
+        Picasso.get().load(bar.image_url).into(barImageView)
         Picasso.get().load(bar.image_url).into(barImageView)
 
         //Set title for the custom info window with bar name from Bar Object (retrieved from Yelp API)
@@ -33,7 +34,7 @@ class CustomInfoWindowAdapter: GoogleMap.InfoWindowAdapter  {
         var phone = bar.phone
         var cost = bar.price
 
-        //Yelp API can return empty and null strings, so replacing with N/a for uniformity
+        //Yelp API can return empty and null strings, so replacing with N/A for uniformity
         if(bar.phone == "" || bar.phone == null) {
             phone = "N/A"
         }
