2b030c51313d8fd6b4695b30ff99203aa0700019
Andrew <aphn490@gmail.com>
2023-04-01 04:24:39 -0700

    Adding functionality to search button (and fixing bug?)

--- app/src/main/java/com/example/drinkmate/Drinks.kt
+++ app/src/main/java/com/example/drinkmate/Drinks.kt
@@ -5,55 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Drinks.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Drinks : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drinks, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DrinksFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Drinks().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val view = inflater.inflate(R.layout.fragment_drinks, container, false)
+        val navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+        val searchButton : Button = view.findViewById(R.id.searchButton)
+        searchButton.setOnClickListener(){
+            // Navigates the fragment to the drink search fragment
+            navController.navigate(R.id.action_drinks_to_drinkSearch)
+        }
+        return view
     }
 }
